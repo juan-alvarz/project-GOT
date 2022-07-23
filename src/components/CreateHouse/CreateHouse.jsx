85637dc90ed1e64a3fd3378bab8b1fa3f3de2b62
juan-alvarz <juanalvarez0925@gmail.com>
2022-07-23 01:57:17 -0500

    it's 1.57AM

--- src/components/CreateHouse/CreateHouse.jsx
+++ src/components/CreateHouse/CreateHouse.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createHouse } from "../../redux/actions";
+import "./CreateHouse.css";
 
 // CUIDADOOOO. SI O SI FUNCTIONAL COMPONENT! SE ROMPEN LOS TEST EN CASO CONTRARIO!!
 // TAMBIEN VAS A TENER QUE USAR HOOKS!
@@ -29,33 +30,55 @@ const CreateHouse = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label>Name: </label>
-        <input
-          type="text"
-          name="name"
-          onChange={inputChange}
-          value={input.name}
-        />
-        <label>Region: </label>
-        <input
-          type="text"
-          name="region"
-          onChange={inputChange}
-          value={input.region}
-        />
-        <label>Words: </label>
-        <input
-          type="text"
-          name="words"
-          onChange={inputChange}
-          value={input.words}
-        />
-        <button type="submit" onClick={() => dispatch(createHouse(input))}>
-          Create
-        </button>
-      </form>
+    <div className="createHouse-container">
+      <div className="containerHouseCreate">
+        <h2 className="tituloContainer">Create House</h2>
+        <p className="textContainer">
+          En este apartado podrás crear tu propia casa de{" "}
+          <strong>Game of Thrones</strong>, solo necesitas llenar los campos
+          requeridos a continuación
+        </p>
+        <form className="createHouse-form" onSubmit={(e) => handleSubmit(e)}>
+          <label className="label-form">Name: </label>
+          <input
+            type="text"
+            name="name"
+            onChange={inputChange}
+            autoComplete={false}
+            value={input.name}
+            className="input-form"
+            placeholder="Name"
+          />
+          <br />
+          <label className="label-form">Region: </label>
+          <input
+            type="text"
+            name="region"
+            onChange={inputChange}
+            value={input.region}
+            className="input-form"
+            placeholder="Region"
+          />
+          <br />
+          <label className="label-form">Words: </label>
+          <input
+            type="text"
+            name="words"
+            onChange={inputChange}
+            value={input.words}
+            className="input-form"
+            placeholder="Words"
+          />
+          <br />
+          <button
+            className="button-createForm"
+            type="submit"
+            onClick={() => dispatch(createHouse(input))}
+          >
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/HouseCard/HouseCard.jsx
+++ src/components/HouseCard/HouseCard.jsx
@@ -2,22 +2,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { deleteHouse } from "../../redux/actions/index.js";
-//first commit
+import "./HouseCard.css";
 
 // CUIDADOOOO. SI O SI CLASS COMPONENT! SE ROMPEN LOS TEST EN CASO CONTRARIO!!
 // TAMBIEN VAS A TENER QUE USAR EL METODO CONNECT DE REDUX , JUNTO A MAP_DISPATCH_TO_PROPS! <3
 export class HouseCard extends Component {
   render() {
     return (
-      <div>
-        <button onClick={() => this.props.deleteHouse(this.props.id)}>
-          Delete This
-        </button>
-        <Link to={`/houses/${this.props.id}`}>
-          <h3>{this.props.name}</h3>
-        </Link>
-        <p>Region: {this.props.region}</p>
-        <p>Words: {this.props.words}</p>
+      <div className="container-houseCard">
+        <div className="head-container">
+          <button
+            className="x"
+            onClick={() => this.props.deleteHouse(this.props.id)}
+          >
+            x
+          </button>
+          <Link className="title-house" to={`/houses/${this.props.id}`}>
+            <h3>{this.props.name}</h3>
+          </Link>
+        </div>
+        <p className="txt-card">Region: {this.props.region}</p>
+        <p className="txt-card">Words: {this.props.words}</p>
       </div>
     );
   }
